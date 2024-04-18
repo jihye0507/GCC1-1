69bc21e1c37f4f22e99a2cce982b2333a514901c
wontae <83wontae@gmail.com>
2024-04-18 13:30:45 +0900

    342

--- UnrealCode/Source/UnrealCode/MoveRect.cpp
+++ UnrealCode/Source/UnrealCode/MoveRect.cpp
@@ -1,18 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "MoveLeftRight.h"
+#include "MoveRect.h"
 
 // Sets default values
-AMoveLeftRight::AMoveLeftRight():IsMoveRight(true), LocX(0)
+AMoveRect::AMoveRect()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	// Scene : 생성해서 넣을 변수
-	// = : 대입 연산자(오른쪽에 있는걸 왼쪽에 넣는다.)
-	// CreateDefaultSubobject : 생성 도구
-	// USceneComponent : 생성할 클래스 지정
-	// TEXT("Scene") : 블루프린트에서 보여질 이름
+
 	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
 
 	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
@@ -24,42 +20,16 @@ AMoveLeftRight::AMoveLeftRight():IsMoveRight(true), LocX(0)
 }
 
 // Called when the game starts or when spawned
-void AMoveLeftRight::BeginPlay()
+void AMoveRect::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
 // Called every frame
-void AMoveLeftRight::Tick(float DeltaTime)
+void AMoveRect::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (IsMoveRight)
-	{
-		// True
-		LocX = LocX + 1;// LocX += 1;
-
-		StaticMesh->SetRelativeLocation(FVector(LocX, 0, 0));
-
-		FVector location = StaticMesh->GetRelativeLocation();
-		if (location.X > 200)
-		{
-			IsMoveRight = false;
-		}
-	}
-	else
-	{
-		// False
-		LocX = LocX - 1;// LocX += 1;
-
-		StaticMesh->SetRelativeLocation(FVector(LocX, 0, 0));
-
-		FVector location = StaticMesh->GetRelativeLocation();
-		if (location.X <= 0)
-		{
-			IsMoveRight = true;
-		}
-	}
 }
 
--- UnrealCode/Source/UnrealCode/MoveRect.h
+++ UnrealCode/Source/UnrealCode/MoveRect.h
@@ -4,16 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "MoveLeftRight.generated.h" // Unreal Class 라이브러리 생성 
+#include "MoveRect.generated.h"
 
-UCLASS()// Unreal 클래스 옵션 입력
-class UNREALCODE_API/*모듈 API 이름*/ AMoveLeftRight : public AActor
+UCLASS()
+class UNREALCODE_API AMoveRect : public AActor
 {
-	GENERATED_BODY() // Unreal 코드 생성 처리
+	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	AMoveLeftRight(); // 클래스가 생성될때 실행
+	AMoveRect();
 
 protected:
 	// Called when the game starts or when spawned
@@ -29,9 +29,4 @@ public:
 
 	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
 	UStaticMeshComponent* StaticMesh;
-
-	UPROPERTY(BlueprintReadOnly)
-	bool IsMoveRight;
-
-	float LocX;
 };
